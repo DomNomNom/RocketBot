f6dd84eff9078b2cbe8364d11d1da26ba34ab984
Dominik Schmid <DominikSchmid93@gmail.com>
2018-01-20 22:01:11 +1100

    Allowed saving of replays

--- mimic_bot.py
+++ mimic_bot.py
@@ -10,18 +10,24 @@ import time
 from collections import deque
 from importlib.machinery import SourceFileLoader
 import numpy as np
+import imp
 
 # bakkes = SourceFileLoader("module.name", os.path.dirname(os.path.realpath(__file__)) + "\\bakkes.py").load_module()
 
-from quicktracer import trace
-# quicktracer = SourceFileLoader("module.name", r"C:\Users\dom\Documents\GitHub\quicktracer\quicktracer\__init__.py").load_module()
-# trace = quicktracer.trace
+# from quicktracer import trace
+quicktracer = SourceFileLoader("module.name", r"C:\Users\dom\Documents\GitHub\quicktracer\quicktracer\__init__.py").load_module()
+trace = quicktracer.trace
 
 
 import historian
 import bakkes
+# import reloader
 from controller_input import controller
+import ctype_utils
 
+imp.reload(ctype_utils)
+imp.reload(historian)
+imp.reload(bakkes)
 
 STATE_RECORD = 'record'
 STATE_MIMIC = 'mimic'
@@ -34,12 +40,17 @@ class Agent:
         self.state = STATE_MIMIC
         self.clear_recording(0.0)
         self.first_time = True
-        self.action_dict = {}
+        self.last_time = None
+        self.history = historian.History()
+        self.history.load(descriptor_file_path=None)  # latest
+        self.mimic_start_time = -1
+
+
 
     def clear_recording(self, time):
         # self.actions = {}  # time since recording begin -> controller_state
         self.record_start_time = time
-        self.history = []
+        self.history = historian.History()
 
     def get_output_vector(self, game_tick_packet):
         time = game_tick_packet.gameInfo.TimeSeconds
@@ -47,32 +58,49 @@ class Agent:
         # State transition
         if self.state == STATE_MIMIC and controller.hat_toggle_west:  # mimic -> record
             self.clear_recording(time)
-            self.bakkes_reset_command = bakkes.convert_tick_packet_to_command(game_tick_packet)
             self.state = STATE_RECORD
         if self.state == STATE_RECORD and not controller.hat_toggle_west:  # record -> mimic
-            self.state = STATE_MIMIC
+            self.history.save()
             self.mimic_start_time = -1
-            self.action_dict = historian.to_action_dict(self.history)
-
-            print ('recording finished. ')
+            print('recording finished. ')
+            self.state = STATE_MIMIC
 
 
         if self.state == STATE_MIMIC:
-            return self.mimic(time)
+            return self.mimic(time, game_tick_packet)
         elif self.state == STATE_RECORD:
             return self.record(time, game_tick_packet)
 
-    def mimic(self, time):
-        keyframe_timestamps = sorted(self.action_dict.keys())
+
+    def mimic(self, time, game_tick_packet):
+        action_dict = self.history.get_action_dict()
+        keyframe_timestamps = sorted(action_dict.keys())
         if len(keyframe_timestamps) < 2:
             return [0] * 8  # No action
         replay_duration = max(keyframe_timestamps)
         if time - self.mimic_start_time > replay_duration:
-            bakkes.rcon(self.bakkes_reset_command)
+            bakkes_reset_command = bakkes.convert_tick_packet_to_command(
+                self.history.get_closest_game_tick_packet(self.history.start_time)
+            )
+            bakkes.rcon(bakkes_reset_command)
             self.mimic_start_time = time
         replay_time = time - self.mimic_start_time
         key = max([keyframe_timestamps[0]] + [ t for t in keyframe_timestamps if t <= replay_time ])
-        player_input = self.action_dict[key]
+        player_input = action_dict[key]
+
+        expected_state = self.history.get_closest_game_tick_packet(replay_time)
+        packet_diff = ctype_utils.struct_rms_deviation(expected_state, game_tick_packet, mask={
+            'gameball': all,
+            'gamecars': all,
+        })
+        def diff(mask):
+            deviation = ctype_utils.struct_rms_deviation(expected_state, game_tick_packet, mask=mask)
+            return min(deviation, 1000000)
+        trace(diff({'gamecars': {0: {'AngularVelocity': all, }}}))
+        trace(diff({'gamecars': {0: {'Velocity': all, }}}))
+        trace(diff({'gamecars': {0: {'Location': all, }}}))
+        # trace(packet_diff)
+
         return [
             player_input.fThrottle,
             player_input.fSteer,
